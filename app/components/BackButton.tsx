98981f51abe1f9e287b3808e6e9d5afd5b95320b
Tran Van Tuan <emailcuaban@gmail.com>
2026-06-01 00:59:48 +0700

    hide back button on home page

--- app/components/BackButton.tsx
+++ app/components/BackButton.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 export default function BackButton() {
   const router = useRouter();
+  const pathname = usePathname();
+
+  // Ẩn ở trang chủ
+  if (pathname === "/") {
+    return null;
+  }
 
   return (
     <button
