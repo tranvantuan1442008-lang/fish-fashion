0a05c58503509c06a2992aea80023550bb3309a4
Tran Van Tuan <emailcuaban@gmail.com>
2026-06-01 00:54:26 +0700

    fix mobile layout

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -99,11 +99,12 @@ export default function ProductsPage() {
         </div>
 
         {/* Nữ */}
-        <h2 className="text-4xl font-bold text-pink-600 mb-6">
-          👩 Thời trang Nữ
-        </h2>
+        {/* Nữ */}
+<h2 className="text-4xl font-bold text-pink-600 mb-6">
+  👩 Thời trang Nữ
+</h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
+<div className="grid grid-cols-2 md:grid-cols-5 gap-4">
 
           <div
             onClick={() =>
