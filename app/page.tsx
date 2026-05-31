3e03c94b83cebe6c5082c91e1b2ec76df3217f2b
Tran Van Tuan <emailcuaban@gmail.com>
2026-06-01 00:40:07 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,44 @@ export default function Home() {
     <main className="min-h-screen bg-white">
 
       <section
-        className="h-[650px] bg-cover bg-center flex items-center justify-center"
-        style={{
-          backgroundImage: "url('/fish-banner.png')",
-        }}
-      >
+  className="
+    min-h-[500px]
+    md:h-[650px]
+    bg-cover
+    bg-center
+    flex
+    items-center
+    justify-center
+    px-4
+  "
+  style={{
+    backgroundImage: "url('/fish-banner.png')",
+  }}
+>
         <div className="text-center">
-          <h1 className="text-6xl font-bold text-blue-700">
+          <h1 className="text-4xl md:text-6xl font-bold text-blue-700">
             FISH FASHION
           </h1>
 
-          <p className="mt-4 text-xl text-gray-700">
+          <p className="mt-4 text-lg md:text-xl text-gray-700">
             Thời trang nam và nữ hiện đại
           </p>
 
           <Link
   href="/products"
-  className="mt-8 inline-block bg-blue-600 text-white px-8 py-4 rounded-full text-xl"
+  className="
+  mt-8
+  inline-block
+  bg-blue-600
+  text-white
+  px-6
+  md:px-8
+  py-3
+  md:py-4
+  rounded-full
+  text-base
+  md:text-xl
+"
 >
   Mua ngay
 </Link>
